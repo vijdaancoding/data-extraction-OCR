55ed2447214fbd8ffb4f4b5040b5fe79f7621c3d
Ali Vijdaan <alivijdaan@gmail.com>
2024-08-05 10:50:07 +0800

    updated streamlit app

--- app.py
+++ app.py
@@ -1,18 +1,59 @@
 import streamlit as st
-from PIL import Image
 import tempfile
 import os 
-import io
-import glob
-import json
 import base64
 
-from extraction_ocr import ocr_extraction
+from extraction_ocr import ocr_extraction, filter_text_elements
 
-# Set the page layout to wide
-st.set_page_config(layout="wide")
+#Create a temporary directory to store uplaoded documents
+def create_temp_dir(uploaded_file): 
+    temp_dir = tempfile.mkdtemp()
+    temp_file_path = os.path.join(temp_dir, uploaded_file.name)
 
-def print_text(raw_elements):
+    with open(temp_file_path, 'wb') as temp_file:
+        temp_file.write(uploaded_file.read())
+
+    output_dir = os.path.join(temp_dir, "output")
+    os.makedirs(output_dir, exist_ok=True)
+
+    return temp_file_path, output_dir
+
+# Main function to perform OCR and text extraction
+def run_ocr(uploaded_file): 
+    text_elements = []
+    raw_elements = []
+
+    if uploaded_file is not None: 
+        temp_file_path, output_dir = create_temp_dir(uploaded_file)
+
+        if uploaded_file.type == 'application/pdf': 
+            raw_elements = ocr_extraction(file_name=temp_file_path, output_dir=output_dir)
+            text_elements = filter_text_elements(raw_elements)
+
+            return text_elements, temp_file_path
+        else: 
+            print("No PDF file found")
+    else: 
+        print("No file found")
+
+def get_base64_of_file(file_path):
+    with open(file_path, "rb") as file:
+        return base64.b64encode(file.read()).decode()
+
+def show_uploaded_docs(uploaded_file, temp_file_path): 
+
+    if uploaded_file is not None: 
+        st.subheader("Doc Viewer")
+
+        if uploaded_file.type == 'application/pdf': 
+            #Display pdf in iframe
+            pdf_base64 = get_base64_of_file(temp_file_path)
+            pdf_display = f'<iframe src="data:application/pdf;base64,{pdf_base64}" height="500" width="100%" type="application/pdf"></iframe>'
+            st.markdown(pdf_display, unsafe_allow_html=True)
+    else:
+        st.write("No file found")
+
+def show_extracted_text(elements):
 
     # Add custom CSS to style the container
     st.markdown(
@@ -20,7 +61,7 @@ def print_text(raw_elements):
         <style>
         .scrollable-container {
             height: 500px;
-            width: 450px;
+            width: 100%;
             overflow-y: scroll;
             border: 1px solid #ccc;
             padding: 10px;
@@ -33,59 +74,40 @@ def print_text(raw_elements):
 
     # Create a container with the custom class
     container_html = '<div class="scrollable-container">'
-    for element in raw_elements:
+    for element in elements:
         container_html += f"<p>{element}</p>"
     container_html += '</div>'
-    
+
     # Render the container with the elements inside
     st.markdown(container_html, unsafe_allow_html=True)
-
-# Function to convert file to base64 for embedding
-def get_base64_of_file(file_path):
-    with open(file_path, "rb") as file:
-        return base64.b64encode(file.read()).decode()
-
-st.title("PDF and Doc Data Extractor")
-
-uploaded_file = st.file_uploader("Choose a PDF", type=["pdf"])
-
-if uploaded_file is not None:
-
-    # Create a temporary directory to save uploaded files
-    with tempfile.TemporaryDirectory() as temp_dir:
-        temp_file_path = os.path.join(temp_dir, uploaded_file.name)
-        
-        with open(temp_file_path, 'wb') as temp_file:
-            temp_file.write(uploaded_file.read())
-        
-        # Create an output directory within the temporary directory
-        output_dir = os.path.join(temp_dir, "output")
-        os.makedirs(output_dir, exist_ok=True)
-
-        # Check if the file is a PDF
-        if uploaded_file.type == "application/pdf":
-            #Call OCR Function
-            raw_elements = ocr_extraction(file_name=temp_file_path, output_dir=output_dir)
-
-        # Create columns for side-by-side display
-        col1, col2 = st.columns([8, 8])
-
-        with col1:
-            # Display the PDF with a scroller
-            st.subheader("PDF Viewer")
-            pdf_base64 = get_base64_of_file(temp_file_path)
-            pdf_display = f'<iframe src="data:application/pdf;base64,{pdf_base64}" height="500" width="450" type="application/pdf"></iframe>'
-            st.markdown(pdf_display, unsafe_allow_html=True)
-
-        with col2:
-            # Display OCR text
-            st.subheader("OCR Results")
-            print_text(raw_elements)
     
-        # Create a download button for each file in the output directory
-        st.write("Download extracted files:")
-        for file_path in glob.glob(os.path.join(output_dir, '*')):
-            file_name = os.path.basename(file_path)
-            with open(file_path, 'rb') as file:
-                file_bytes = file.read()
-                st.download_button(label=f"Download {file_name}", data=file_bytes, file_name=file_name)
+
+def main(): 
+    st.set_page_config(page_title="Doc Extractor", layout="wide")
+    
+    with st.container():    
+        st.title("Document Data Extractor")
+        st.write("The goal is to create an ETL pipeline that can load documents and preprocess them to make them RAG Ready!")
+
+    uploaded_file = st.file_uploader("Choose a file", type=["pdf"])
+
+    if uploaded_file is not None: 
+        text_elements, temp_file_path = run_ocr(uploaded_file)
+
+        with st.container():
+            left_column, right_column = st.columns(2)
+
+            with left_column: 
+                with st.expander("Show original document"): 
+                    show_uploaded_docs(uploaded_file, temp_file_path)
+
+            with right_column: 
+                with st.expander("Show Extracted Text"):
+                    show_extracted_text(text_elements)
+                    
+
+
+
+
+if __name__ == "__main__": 
+    main()
--- extraction_ocr.py
+++ extraction_ocr.py
@@ -5,6 +5,7 @@ from unstructured.partition.pdf import partition_pdf
 #file_name = r"Corporate Comprehensive EDI.pdf"
 #output_dir = "Image Extractions"
 
+#Function for OCR Extraction from PDFs
 def ocr_extraction(file_name, output_dir): 
     raw_elements = partition_pdf(
         filename=file_name,
@@ -16,3 +17,27 @@ def ocr_extraction(file_name, output_dir):
 
     return raw_elements
 
+#Filtering Text Elements 
+text_element_items = [unstructured.documents.elements.Text, 
+                      unstructured.documents.elements.NarrativeText,
+                      unstructured.documents.elements.ListItem, 
+                      unstructured.documents.elements.Header, 
+                      unstructured.documents.elements.Footer, 
+                      unstructured.documents.elements.Title, 
+                      unstructured.documents.elements.CompositeElement]
+
+image_element_items = [unstructured.documents.elements.Image]
+
+def filter_text_elements(raw_elements): 
+    text_elements = []
+
+    for ele in raw_elements:
+        if type(ele) in text_element_items: 
+            text_elements.append(ele)
+        else: continue
+
+    return text_elements
+    
+    
+
+    
